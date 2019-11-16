009fc8f792cab0a86ba71bcbb1dc0569f2735e7b
xufan <3002862704@qq.com>
2019-11-16 17:56:06 +0800

    修改启动路径

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const logger = require("koa-logger")
 const InitManager = require('./app/lib/core.js')
 const catchError = require('./app/middlewares/exceptions')
 const app = new koa();
+
 app.use(cors()) //配置跨域
 app.use(catchError())
 app.use(bodyParser())
 app.use(json())
 app.use(logger())
+
 
 
 InitManager.initApp(app)
